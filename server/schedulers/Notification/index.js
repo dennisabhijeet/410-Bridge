a5443cfca9ad2e2dbe3fca6d32f043f189fc0bf3
Mir Ayman Ali <alimirayman@outlook.com>
2020-09-17 23:04:33 +0600

    fix notification time and muti send

--- server/schedulers/Notification/index.js
+++ server/schedulers/Notification/index.js
@@ -1,6 +1,7 @@
 const { CronJob } = require('cron')
 const moment = require('moment')
-
+const AWS = require('aws-sdk')
+const logger = require('../../util/logger')
 const {
   getAnnouncementsInRange,
   getFomatedAnnouncementsAndUser,
@@ -22,9 +23,82 @@ const cronFunction = async function () {
     console.error(err)
   }
 }
+
+AWS.config.update({ region: 'us-east-1' }) // change to your region
+var opts = {
+  credentials: new AWS.EC2MetadataCredentials(), // default to use the credentials for the ec2 instance
+}
+
+var elasticbeanstalk = new AWS.ElasticBeanstalk(opts)
+var ec2 = new AWS.EC2(opts)
+var metadata = new AWS.MetadataService(opts)
+
+function runTaskOnMaster(taskToRun) {
+  return new Promise((resolve, reject) => {
+    metadata.request('/latest/meta-data/instance-id', (err, InstanceId) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(InstanceId)
+    })
+  })
+    .then((currentInstanceId) => {
+      // console.log('InstanceId', currentInstanceId);
+      return new Promise((resolve, reject) => {
+        var params = {
+          Filters: [
+            {
+              Name: 'resource-id',
+              Values: [currentInstanceId],
+            },
+          ],
+        }
+
+        ec2.describeTags(params, (err, data) => {
+          if (err) {
+            return reject('dt' + err)
+          }
+
+          var envIdTag = data.Tags.find(
+            (t) => t.Key === 'elasticbeanstalk:environment-id'
+          )
+          if (envIdTag === null) {
+            return reject(
+              'Failed to find the value of "elasticbeanstalk:environment-id" tag.'
+            )
+          }
+
+          elasticbeanstalk.describeEnvironmentResources(
+            { EnvironmentId: envIdTag.Value },
+            function (err, data) {
+              if (err) {
+                return reject('de' + err)
+              }
+              if (
+                currentInstanceId !== data.EnvironmentResources.Instances[0].Id
+              ) {
+                return resolve(false)
+              }
+              return resolve(true)
+            }
+          )
+        })
+      })
+    })
+    .then((isMaster) => {
+      if (!isMaster) {
+        // logger.warn('Not running task as not master EB instance.');
+      } else {
+        // logger.info('Identified as master EB instance. Running task.');
+        taskToRun()
+      }
+    })
+    .catch((err) => logger.error(err))
+}
+
 var job = new CronJob(
-  '0 7,12,18 * * *',
-  cronFunction,
+  '30 12,17,23 * * *',
+  runTaskOnMaster(cronFunction),
   null
   // true
 )
